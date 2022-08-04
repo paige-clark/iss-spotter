bf1d3a01843f7321d121a2ecf7a15a9d338e17e4
paige-clark <paigeclarkillustration@gmail.com>
2022-08-04 21:37:02 +0000

    Part 1 files

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP(fetchMyIP())
